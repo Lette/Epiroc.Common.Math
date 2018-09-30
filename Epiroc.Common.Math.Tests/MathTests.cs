388c43554e6dd34080155e0f9030f7615ce89a3c
Christoffer Lette <christoffer.lette@gmail.com>
2018-10-01 00:44:17 +0200

    Initial commit

---
+++ Epiroc.Common.Math.Tests/MathTests.cs
@@ -0,0 +1,112 @@
+﻿using Xunit;
+
+namespace Epiroc.Common.Math.Tests
+{
+    public class MathTests
+    {
+        [Fact]
+        public void Two_different_points_are_not_equal()
+        {
+            var point1 = new Point2D(1.0, 1.0);
+            var point2 = new Point2D(2.0, 2.0);
+
+            Assert.NotEqual(point1, point2);
+        }
+
+        [Fact]
+        public void Point_is_equal_to_self()
+        {
+            var point = new Point2D(2.0, 3.0);
+
+            Assert.Equal(point, point);
+        }
+
+        [Fact]
+        public void Points_with_same_coordinates_are_equal()
+        {
+            var point1 = new Point2D(1.5, 1.5);
+            var point2 = new Point2D(1.5, 1.5);
+
+            Assert.Equal(point1, point2);
+        }
+
+        [Fact]
+        public void Can_read_X_coordinate()
+        {
+            var point = new Point2D(1.0, 2.0);
+
+            Assert.Equal(1.0, point.X);
+        }
+
+        [Fact]
+        public void Can_read_Y_coordinate()
+        {
+            var point = new Point2D(1.0, 2.0);
+
+            Assert.Equal(2.0, point.Y);
+        }
+
+        [Fact]
+        public void Can_add_two_vectors()
+        {
+            var v1 = new Vector2D(1.0, 2.0);
+            var v2 = new Vector2D(0.1, 0.3);
+
+            var result = v1 + v2;
+
+            Assert.Equal(new Vector2D(1.1, 2.3), result);
+        }
+
+        [Fact]
+        public void Subtracting_two_points_creates_a_vector()
+        {
+            var point1 = new Point2D(2.0, 3.0);
+            var point2 = new Point2D(1.0, 1.0);
+
+            var result = point1 - point2;
+
+            Assert.Equal(new Vector2D(1.0, 2.0), result);
+        }
+
+        [Fact]
+        public void Can_subtract_two_vectors()
+        {
+            var v1 = new Vector2D(3.0, 4.0);
+            var v2 = new Vector2D(2.0, 1.0);
+
+            var result = v1 - v2;
+            
+            Assert.Equal(new Vector2D(1.0, 3.0), result);
+        }
+
+        [Fact]
+        public void Can_multiply_a_vector_with_a_scalar()
+        {
+            var v1 = new Vector2D(-1.0, 2.0);
+
+            var result = 1.5 * v1;
+
+            Assert.Equal(new Vector2D(-1.5, 3.0), result);
+        }
+
+        [Fact]
+        public void Can_get_vector_length()
+        {
+            var v = new Vector2D(3.0, 4.0);
+
+            var result = v.Length();
+
+            Assert.Equal(5.0, result);
+        }
+
+        [Fact]
+        public void Can_get_length_of_line()
+        {
+            var line = new Line2D(new Point2D(2.0, 2.0), new Point2D(7.0, 14.0));
+
+            var result = line.Length();
+
+            Assert.Equal(13.0, result);
+        }
+    }
+}
---
+++ Epiroc.Common.Math/Math.fs
@@ -0,0 +1,66 @@
+namespace Epiroc.Common.Math
+
+open System.Diagnostics
+
+[<DebuggerDisplay("[{X}, {Y}]")>]
+[<Struct>]
+type Vector2D (x : float, y : float) =
+    member __.X = x
+    member __.Y = y
+    override __.ToString () = sprintf "[%g, %g]" x y
+
+module Vector2D =
+    let internal create x y = Vector2D (x, y)
+    let (|Vector2D|) (v : Vector2D) = (v.X, v.Y)
+
+    let zero  = create 0.0 0.0
+    let unitX = create 1.0 0.0
+    let unitY = create 0.0 1.0
+
+    let add            (Vector2D (x1, y1)) (Vector2D (x2, y2)) = create (x1 + x2) (y1 + y2)
+    let subtract       (Vector2D (x1, y1)) (Vector2D (x2, y2)) = create (x1 - x2) (y1 - y2)
+    let scalarMultiply k                   (Vector2D (x, y))   = create (k * x)   (k * y)
+
+    let length (v : Vector2D) = sqrt (v.X ** 2.0 + v.Y ** 2.0)
+
+type Vector2D with
+    static member Zero = Vector2D.zero
+    static member UnitVectorX = Vector2D.unitX
+    static member UnitVectorY = Vector2D.unitY
+
+    static member (+) (v1, v2) = Vector2D.add v1 v2
+    static member (-) (v1, v2) = Vector2D.subtract v1 v2
+    static member (*) (k, v) = Vector2D.scalarMultiply k v
+
+    member v.Length () = Vector2D.length v
+
+[<Struct>]
+[<DebuggerDisplay("({X}, {Y})")>]
+type Point2D (x : float, y : float) =
+    member __.X = x
+    member __.Y = y
+    override __.ToString () = sprintf "(%g, %g)" x y
+
+module Point2D =
+    let internal create x y = Point2D (x, y)
+    let (|Point2D|) (point : Point2D) = (point.X, point.Y)
+    let zero = create 0.0 0.0
+    let subtract (Point2D (x1, y1)) (Point2D (x2, y2)) = Vector2D.create (x1 - x2) (y1 - y2)
+
+type Point2D with
+    static member Zero = Point2D.zero
+    static member (-) (p1, p2) = Point2D.subtract p1 p2
+
+[<Struct>]
+[<DebuggerDisplay("({P1}, {P2})")>]
+type Line2D (p1 : Point2D, p2 : Point2D) =
+    member __.P1 = p1
+    member __.P2 = p2
+    override __.ToString () = sprintf "(%A - %A)" p1 p2
+
+module Line2D =
+    let (|Line2D|) (l : Line2D) = (l.P1, l.P2)
+    let length (Line2D (p1, p2)) = Point2D.subtract p1 p2 |> Vector2D.length
+
+type Line2D with
+    member line.Length () = Line2D.length line

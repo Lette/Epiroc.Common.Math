022641a0e8a3e5b0dd82ea79163a7dc25c91860d
Christoffer Lette <christoffer.lette@gmail.com>
2018-10-03 19:36:40 +0200

    More attributes

--- Epiroc.Common.Math/AssemblyInfo.fs
+++ Epiroc.Common.Math/AssemblyInfo.fs
@@ -1,6 +1,7 @@
 ﻿namespace Epiroc.Common.Math
 
 [<assembly: System.Reflection.AssemblyVersion("0.1.0.0")>]
-//[<assembly: System.Reflection.AssemblyFileVersion("0.1.0.0")>]
+[<assembly: System.Reflection.AssemblyFileVersion("0.1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0.0")>]
 
 do ()
